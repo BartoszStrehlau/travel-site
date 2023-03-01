82c26525cbb2d5010c3e7c406fc3eb8b9435b95d
Shyeline <bartekstrehlau@gmail.com>
2023-03-01 22:55:43 +0100

    made some ajustments

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,22 @@
 import '../styles/styles.css'
 
+// eslint-disable-next-line no-undef
 if (module.hot) {
+    // eslint-disable-next-line no-undef
     module.hot.accept()
 }
+
+// Lesson example code below this line  
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.color = favColor;
+    this.greet = function() {
+        console.log('Oh Hello! My name is '+this.name+' and my favourite color is '+this.color+'.');
+    }
+}
+
+let john = new Person('John Doe', 'blue');
+john.greet();
+let jane = new Person('Jone Smith', 'green');
+jane.greet();
