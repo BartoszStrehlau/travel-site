b09774fd485cdd039a8c18fdde4cf22b09c2ff3b
Shyeline <bartekstrehlau@gmail.com>
2023-03-07 00:35:38 +0100

    Completed reveal on scroll for testimonial and our features area. Also made keyframes a seperate file for ease of work

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,15 @@
 import '../styles/styles.css'
 import { MobileMenu } from './modules/MobileMenu.js'
+import { RevealOnScroll } from './modules/RevealOnScroll'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let mobileMenu = new MobileMenu()
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
+
 
 // eslint-disable-next-line no-undef
 if (module.hot) {
     // eslint-disable-next-line no-undef
     module.hot.accept()
 }
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let mobileMenu = new MobileMenu();
-
