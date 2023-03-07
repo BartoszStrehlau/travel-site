102eb7e6c9e106a3f1f19887df9308790af20d6b
Shyeline <bartekstrehlau@gmail.com>
2023-03-08 00:25:36 +0100

    Completed Sticky Header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css'
 import { MobileMenu } from './modules/MobileMenu.js'
 import { RevealOnScroll } from './modules/RevealOnScroll'
+import { StickyHeader } from './modules/StickyHeader'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 let mobileMenu = new MobileMenu()
+let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 
