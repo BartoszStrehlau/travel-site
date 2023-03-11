3b6bed9cdfc9f171a11674326daa026116c2e760
Shyeline <bartekstrehlau@gmail.com>
2023-03-11 21:43:52 +0100

    made links glow up when scrolled to section

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,20 +1,53 @@
-import { throttle } from "lodash";
+import { throttle, debounce } from "lodash";
 
 export class StickyHeader {
     constructor() {
         this.siteHeader = document.querySelector('.site-header');
+        this.pageSections= document.querySelectorAll('.page-section');
+        this.browserHeight = window.innerHeight;
+        this.previousScrollY =window.scrollY;
         this.events();
     }
 
     events() {
         window.addEventListener('scroll', throttle(() => this.runOnScroll(), 200))
+        window.addEventListener('resize', debounce(() => {
+            console.log("Resize just run")
+            this.browserHeight = window.innerHeight
+            }, 333)
+        )
     }
 
     runOnScroll(){
+        this.determineScrollDirection()
+
         if (window.scrollY > 60) {
             this.siteHeader.classList.add('site-header--darker');
         } else {
             this.siteHeader.classList.remove('site-header--darker');
+        }
+
+        this.pageSections.forEach(el => this.callSections(el))
+    }
+
+    determineScrollDirection() {
+        if (window.scrollY > this.previousScrollY) {
+            this.scrollDirection = 'down'
+        } else {
+            this.scrollDirection = 'up'
+        }
+
+        this.previousScrollY = window.scrollY
+    }
+
+    callSections(el) {
+        if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
+            let scrollPercent = el.getBoundingClientRect().top / this.browserHeight * 100;
+            if (scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 33 && this.scrollDirection == 'up') {
+                let matchingLink = el.getAttribute('data-matching-link');
+                document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove('is-current-link'))
+                document.querySelector(matchingLink).classList.add('is-current-link');
+            }
         }
     }
 }
