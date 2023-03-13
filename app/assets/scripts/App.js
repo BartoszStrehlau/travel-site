5f324ab714a100f450d7a521c26178403332f7e9
Shyeline <62762388+BartoszStrehlau@users.noreply.github.com>
2023-03-13 18:13:34 +0000

    made clicking the get started button open the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,13 @@ import '../styles/styles.css'
 import { MobileMenu } from './modules/MobileMenu.js'
 import { RevealOnScroll } from './modules/RevealOnScroll'
 import { StickyHeader } from './modules/StickyHeader'
+import { Modal } from './modules/_Modal'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+new Modal()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let mobileMenu = new MobileMenu()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
