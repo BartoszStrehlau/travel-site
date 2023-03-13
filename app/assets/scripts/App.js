dfc8bb299f72db4a660836add11be97caacdd75c
Shyeline <62762388+BartoszStrehlau@users.noreply.github.com>
2023-03-13 22:49:18 +0100

    Maybe that's the fix?

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -16,8 +16,8 @@ document.querySelectorAll('.open-modal').forEach(el => {
     el.addEventListener('click', (event) => {
         event.preventDefault();
         if(typeof modal == 'undefined'){
-            import(/* webpackChunkName: "modal" */ /* webpackExports: ["Modal"] */ './modules/Modal').then(x => {
-                modal = new x.Modal()
+            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default;
                 setTimeout(() => modal.openTheModal(), 20)
             }).catch(() => console.log("There was a problem importing the Modal module"))
         }else{
