418572a8beadc3c36531332741f3d4ce52a58b0a
Shyeline <62762388+BartoszStrehlau@users.noreply.github.com>
2023-03-16 17:04:28 +0000

    changed the thing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const currentTask = process.env.npm_lifecycle_event
 const path =require('path')
+
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -7,22 +9,8 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-module.exports = {
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
     module: {
         rules: [
             {
@@ -32,6 +20,34 @@ module.exports = {
         ]
     }
 }
+
+if (currentTask == 'dev'){
+
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+    config.mode = "development"
+}
+
+if (currentTask == 'build'){
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    config.mode = 'production'
+}
+
+module.export = config
 // newest webpack config for devserver module
 //  devServer: {
 //      watchFiles: ('./app/**/*.html'),
